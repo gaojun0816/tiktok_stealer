482e577d946c28f63f4da939b75906268c6eea1d
Jun GAO <gaojun0816@yahoo.com>
2020-02-21 10:37:54 +0100

    updated gitignore

---
+++ app/src/main/java/lu/uni/jungao/plagiarist/AntiTheftActivity.java
@@ -0,0 +1,13 @@
+package lu.uni.jungao.plagiarist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AntiTheftActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anti_theft);
+    }
+}
